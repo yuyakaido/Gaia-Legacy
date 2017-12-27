f7ae01b5bffd9c8fbe37580e02d280c18654cbbd
Yuya Kaido <yuyakaido@gmail.com>
2017-12-27 15:15:06 +0900

    Delete legacy code

--- app/src/main/java/com/yuyakaido/android/blueprint/di/module/DomainModule.java
+++
@@ -1,9 +0,0 @@
-package com.yuyakaido.android.blueprint.di.module;
-
-import dagger.Module;
-
-/**
- * Created by yuyakaido on 3/5/16.
- */
-@Module
-public class DomainModule {}
--- app/src/test/java/com/yuyakaido/android/blueprint/presentation/PresentationTest.java
+++
@@ -1,8 +0,0 @@
-package com.yuyakaido.android.blueprint.presentation;
-
-import com.yuyakaido.android.blueprint.app.GenesisTest;
-
-/**
- * Created by yuyakaido on 3/13/16.
- */
-public abstract class PresentationTest extends GenesisTest {}
