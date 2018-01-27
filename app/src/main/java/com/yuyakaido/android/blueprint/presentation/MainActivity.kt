aa4b7d93fb887121202cd2066f4708ae1951c5f9
Yuya Kaido <yuyakaido@gmail.com>
2018-01-27 21:09:58 +0900

    Move login button to toolbar

--- app/src/main/java/com/yuyakaido/android/blueprint/presentation/MainActivity.kt
+++ app/src/main/java/com/yuyakaido/android/blueprint/presentation/MainActivity.kt
@@ -7,6 +7,9 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Spinner
@@ -14,7 +17,7 @@ import com.twitter.sdk.android.core.Callback
 import com.twitter.sdk.android.core.Result
 import com.twitter.sdk.android.core.TwitterException
 import com.twitter.sdk.android.core.TwitterSession
-import com.twitter.sdk.android.core.identity.TwitterLoginButton
+import com.twitter.sdk.android.core.identity.TwitterAuthClient
 import com.yuyakaido.android.blueprint.R
 import com.yuyakaido.android.blueprint.app.Blueprint
 import com.yuyakaido.android.blueprint.domain.RunningSession
@@ -26,7 +29,7 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    private val loginButton by lazy { findViewById<TwitterLoginButton>(R.id.button) }
+    private val twitterAuthClient = TwitterAuthClient()
     private val spinner by lazy { findViewById<Spinner>(R.id.spinner) }
     private val adapter by lazy { TwitterAccountAdapter(this, running) }
 
@@ -38,12 +41,23 @@ class MainActivity : AppCompatActivity() {
         (application as Blueprint).component.inject(this)
         setContentView(R.layout.activity_main)
         setupToolbar()
-        setupLoginButton()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        loginButton.onActivityResult(requestCode, resultCode, data)
+        twitterAuthClient.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_add_account -> authorize()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupToolbar() {
@@ -56,34 +70,10 @@ class MainActivity : AppCompatActivity() {
                 val target = running.sessions()[position]
                 if (running.current()?.twitter?.id != target.twitter.id) {
                     running.switchTo(position)
-                    switchTwitter()
+                    refresh()
                 }
             }
             override fun onNothingSelected(parent: AdapterView<*>) {}
-        }
-    }
-
-    private fun setupLoginButton() {
-        loginButton.callback = object : Callback<TwitterSession>() {
-            override fun success(result: Result<TwitterSession>) {
-                val session = Session(result.data, application)
-                running.add(session)
-                spinner.setSelection(running.sessions().indexOf(session))
-                adapter.notifyDataSetChanged()
-                switchTwitter()
-            }
-            override fun failure(exception: TwitterException) {
-                Log.e("Blueprint", exception.toString())
-            }
-        }
-    }
-
-    private fun switchTwitter() {
-        running.current()?.let { current ->
-            current.client.homeTimeline(current.twitter)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { setupRecyclerView(it) }
         }
     }
 
@@ -91,6 +81,30 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.adapter = TweetAdapter(this, tweets)
+    }
+
+    private fun authorize() {
+        twitterAuthClient.authorize(this, object : Callback<TwitterSession>() {
+            override fun success(result: Result<TwitterSession>) {
+                val session = Session(result.data, application)
+                running.add(session)
+                spinner.setSelection(running.sessions().indexOf(session))
+                adapter.notifyDataSetChanged()
+                refresh()
+            }
+            override fun failure(exception: TwitterException) {
+                Log.e("Blueprint", exception.toString())
+            }
+        })
+    }
+
+    private fun refresh() {
+        running.current()?.let { current ->
+            current.client.homeTimeline(current.twitter)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { setupRecyclerView(it) }
+        }
     }
 
 }
