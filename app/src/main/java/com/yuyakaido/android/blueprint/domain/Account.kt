9e79f6651d7d02ea47700745d8ea5e13f086d26d
Yuya Kaido <yuyakaido@gmail.com>
2018-01-28 18:15:03 +0900

    Move save/delete methods in account

--- app/src/main/java/com/yuyakaido/android/blueprint/domain/Account.kt
+++ app/src/main/java/com/yuyakaido/android/blueprint/domain/Account.kt
@@ -1,6 +1,9 @@
 package com.yuyakaido.android.blueprint.domain
 
 import android.app.Application
+import android.content.Context
+import android.preference.PreferenceManager
+import com.twitter.sdk.android.core.TwitterAuthToken
 import com.twitter.sdk.android.core.TwitterSession
 import com.yuyakaido.android.blueprint.app.Blueprint
 import com.yuyakaido.android.blueprint.di.AccountModule
@@ -13,6 +16,24 @@ class Account(
         val application: Application,
         val disposables: CompositeDisposable = CompositeDisposable()) {
 
+    companion object {
+        const val TOKEN = "token"
+        const val SECRET = "secret"
+        const val USER_ID = "user_id"
+        const val USER_NAME = "user_name"
+
+        fun load(application: Application, id: String): Account {
+            val accountPreference = application.getSharedPreferences(id, Context.MODE_PRIVATE)
+            val token = accountPreference.getString(TOKEN, null)
+            val secret = accountPreference.getString(SECRET, null)
+            val userId = accountPreference.getLong(USER_ID, 0L)
+            val userName = accountPreference.getString(USER_NAME, null)
+            val autoToken = TwitterAuthToken(token, secret)
+            val session = TwitterSession(autoToken, userId, userName)
+            return Account(session, application)
+        }
+    }
+
     @Inject
     lateinit var repository: TwitterRepository
 
@@ -20,6 +41,43 @@ class Account(
         (application as Blueprint).component
                 .newAccountComponent(AccountModule(this))
                 .inject(this)
+    }
+
+    fun save() {
+        val accountPreference = application.getSharedPreferences(
+                twitter.userId.toString(),
+                Context.MODE_PRIVATE)
+        accountPreference.edit()
+                .apply {
+                    putString(TOKEN, twitter.authToken.token)
+                    putString(SECRET, twitter.authToken.secret)
+                    putLong(USER_ID, twitter.userId)
+                    putString(USER_NAME, twitter.userName)
+                }
+                .apply()
+
+        val preference = PreferenceManager.getDefaultSharedPreferences(application)
+        val accounts = preference.getStringSet(LoggedInAccount.ACCOUNTS, hashSetOf())
+        accounts.add(twitter.userId.toString())
+        preference.edit().clear().apply()
+        preference.edit().putStringSet(LoggedInAccount.ACCOUNTS, accounts).apply()
+    }
+
+    fun delete() {
+        val accountPreference = application.getSharedPreferences(
+                twitter.userId.toString(),
+                Context.MODE_PRIVATE)
+        accountPreference.edit().clear().apply()
+
+        val preference = PreferenceManager.getDefaultSharedPreferences(application)
+        val accounts = preference.getStringSet(LoggedInAccount.ACCOUNTS, hashSetOf())
+        accounts.remove(twitter.userId.toString())
+        preference.edit().clear().apply()
+        preference.edit().putStringSet(LoggedInAccount.ACCOUNTS, accounts).apply()
+    }
+
+    fun onLoggedOut() {
+        disposables.dispose()
     }
 
 }
--- app/src/main/java/com/yuyakaido/android/blueprint/domain/LoggedInAccount.kt
+++ app/src/main/java/com/yuyakaido/android/blueprint/domain/LoggedInAccount.kt
@@ -1,76 +1,26 @@
 package com.yuyakaido.android.blueprint.domain
 
 import android.app.Application
-import android.content.Context
 import android.preference.PreferenceManager
 import com.jakewharton.rxrelay2.BehaviorRelay
-import com.twitter.sdk.android.core.TwitterAuthToken
-import com.twitter.sdk.android.core.TwitterSession
 import com.yuyakaido.android.blueprint.misc.Pack
 import io.reactivex.Observable
 import javax.inject.Inject
 
-class LoggedInAccount @Inject constructor(
-        private val application: Application) {
+class LoggedInAccount @Inject constructor(application: Application) {
 
     companion object {
         const val ACCOUNTS = "accounts"
-        const val TOKEN = "token"
-        const val SECRET = "secret"
-        const val USER_ID = "user_id"
-        const val USER_NAME = "user_name"
-    }
 
-    private val accounts = BehaviorRelay.createDefault(loadAccounts())
-    private var current = BehaviorRelay.createDefault(Pack(accounts.value.firstOrNull()))
-
-    private fun loadAccounts(): List<Account> {
-        val preference = PreferenceManager.getDefaultSharedPreferences(application)
-        val accounts = preference.getStringSet(ACCOUNTS, hashSetOf())
-        return accounts.map {
-            val accountPreference = application.getSharedPreferences(it, Context.MODE_PRIVATE)
-            val token = accountPreference.getString(TOKEN, null)
-            val secret = accountPreference.getString(SECRET, null)
-            val userId = accountPreference.getLong(USER_ID, 0L)
-            val userName = accountPreference.getString(USER_NAME, null)
-            val autoToken = TwitterAuthToken(token, secret)
-            val session = TwitterSession(autoToken, userId, userName)
-            return@map Account(session, application)
+        private fun accounts(application: Application): List<Account> {
+            val preference = PreferenceManager.getDefaultSharedPreferences(application)
+            val accounts = preference.getStringSet(ACCOUNTS, hashSetOf())
+            return accounts.map { Account.load(application, it) }
         }
     }
 
-    private fun save(account: Account) {
-        val accountPreference = application.getSharedPreferences(
-                account.twitter.userId.toString(),
-                Context.MODE_PRIVATE)
-        accountPreference.edit()
-                .apply {
-                    putString(TOKEN, account.twitter.authToken.token)
-                    putString(SECRET, account.twitter.authToken.secret)
-                    putLong(USER_ID, account.twitter.userId)
-                    putString(USER_NAME, account.twitter.userName)
-                }
-                .apply()
-
-        val preference = PreferenceManager.getDefaultSharedPreferences(application)
-        val accounts = preference.getStringSet(ACCOUNTS, hashSetOf())
-        accounts.add(account.twitter.userId.toString())
-        preference.edit().clear().apply()
-        preference.edit().putStringSet(ACCOUNTS, accounts).apply()
-    }
-
-    private fun delete(account: Account) {
-        val accountPreference = application.getSharedPreferences(
-                account.twitter.userId.toString(),
-                Context.MODE_PRIVATE)
-        accountPreference.edit().clear().apply()
-
-        val preference = PreferenceManager.getDefaultSharedPreferences(application)
-        val accounts = preference.getStringSet(ACCOUNTS, hashSetOf())
-        accounts.remove(account.twitter.userId.toString())
-        preference.edit().clear().apply()
-        preference.edit().putStringSet(ACCOUNTS, accounts).apply()
-    }
+    private val accounts = BehaviorRelay.createDefault(accounts(application))
+    private var current = BehaviorRelay.createDefault(Pack(accounts.value.firstOrNull()))
 
     fun current(): Observable<Pack<Account?>> {
         return current
@@ -89,13 +39,14 @@ class LoggedInAccount @Inject constructor(
             }
         }
 
-        save(account)
+        account.save()
         accounts.accept(accounts.value.plus(account))
         current.accept(Pack(account))
     }
 
     fun remove(account: Account) {
-        delete(account)
+        account.onLoggedOut()
+        account.delete()
         accounts.accept(accounts.value.minus(account))
         if (accounts.value.isEmpty()) {
             current.accept(Pack(null))
