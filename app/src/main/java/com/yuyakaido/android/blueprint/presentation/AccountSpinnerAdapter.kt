be819e32b3edc46ebffe7398520de6652783ff94
Yuya Kaido <yuyakaido@gmail.com>
2018-02-04 14:51:31 +0900

    Refactor account spinner

--- app/src/main/java/com/yuyakaido/android/blueprint/presentation/AccountSpinnerAdapter.kt
+++ app/src/main/java/com/yuyakaido/android/blueprint/presentation/AccountSpinnerAdapter.kt
@@ -11,18 +11,18 @@ import com.yuyakaido.android.blueprint.domain.Account
 class AccountSpinnerAdapter(
         private val context: Context) : BaseAdapter() {
 
-    private var sessions = listOf<Account>()
+    private var accounts = listOf<Account>()
 
     override fun getCount(): Int {
-        return sessions.size
+        return accounts.size
     }
 
     override fun getItemId(position: Int): Long {
-        return sessions[position].twitter.id
+        return accounts[position].twitter.id
     }
 
     override fun getItem(position: Int): Account {
-        return sessions[position]
+        return accounts[position]
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -33,19 +33,19 @@ class AccountSpinnerAdapter(
             convertView.tag as ItemAccountSpinnerBinding
         }
 
-        val session = getItem(position)
-        binding.name.text = session.twitter.userName
+        val account = getItem(position)
+        binding.name.text = account.twitter.userName
 
         return binding.root
     }
 
     fun replace(accounts: List<Account>) {
-        this.sessions = accounts
+        this.accounts = accounts
         notifyDataSetChanged()
     }
 
     fun indexOf(account: Account): Int {
-        return sessions.indexOfFirst { it.twitter.userId == account.twitter.id }
+        return accounts.indexOfFirst { it == account }
     }
 
 }
