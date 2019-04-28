e2db06a4c976fa1d7ce20a88822c2d1354c0b0ee
Yuya Kaido <yuyakaido@gmail.com>
2019-04-28 17:40:05 +0900

    Remove needless bind method for dagger

--- ui/src/main/java/com/yuyakaido/android/gaia/ui/di/MainFragmentModule.kt
+++ ui/src/main/java/com/yuyakaido/android/gaia/ui/di/MainFragmentModule.kt
@@ -1,32 +1,23 @@
 package com.yuyakaido.android.gaia.ui.di
 
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.yuyakaido.android.gaia.ui.MainFragment
 import com.yuyakaido.android.gaia.ui.MainViewModel
 import com.yuyakaido.android.gaia.ui.MainViewModelFactory
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-abstract class MainFragmentModule {
+class MainFragmentModule {
 
-    @Binds
-    abstract fun bindsFragment(fragment: MainFragment): Fragment
-
-    @Module
-    companion object {
-        @JvmStatic
-        @Provides
-        fun provideMainViewModel(
-            fragment: MainFragment,
-            factory: MainViewModelFactory
-        ): MainViewModel {
-            return ViewModelProviders
-                .of(fragment, factory)
-                .get(MainViewModel::class.java)
-        }
+    @Provides
+    fun provideMainViewModel(
+        fragment: MainFragment,
+        factory: MainViewModelFactory
+    ): MainViewModel {
+        return ViewModelProviders
+            .of(fragment, factory)
+            .get(MainViewModel::class.java)
     }
 
 }
