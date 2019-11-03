c73114716e258d204b324a478001a9883af536ca
Yuya Kaido <yuyakaido@gmail.com>
2019-11-03 18:19:24 +0900

    Apply code style

--- app/src/main/java/com/yuyakaido/android/gaia/MainActivity.kt
+++ app/src/main/java/com/yuyakaido/android/gaia/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 
 }
