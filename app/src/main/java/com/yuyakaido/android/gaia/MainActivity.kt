785c3eedc3641dc3b68cfcb4695079592bf05681
Yuya Kaido <yuyakaido@gmail.com>
2019-11-03 18:24:23 +0900

    Add data binding

--- app/src/main/java/com/yuyakaido/android/gaia/MainActivity.kt
+++ app/src/main/java/com/yuyakaido/android/gaia/MainActivity.kt
@@ -2,12 +2,15 @@ package com.yuyakaido.android.gaia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.yuyakaido.android.gaia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+  private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(binding.root)
   }
 
 }
