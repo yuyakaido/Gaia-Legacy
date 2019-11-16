52faffcf03c3108e865da1e3cac1c6decb360038
Yuya Kaido <yuyakaido@gmail.com>
2019-11-17 03:47:27 +0900

    Show placeholder when loading image

--- app/src/main/java/com/yuyakaido/android/gaia/SubredditItem.kt
+++ app/src/main/java/com/yuyakaido/android/gaia/SubredditItem.kt
@@ -1,5 +1,7 @@
 package com.yuyakaido.android.gaia
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import com.bumptech.glide.Glide
 import com.xwray.groupie.databinding.BindableItem
@@ -9,16 +11,21 @@ class SubredditItem(
   private val subreddit: Subreddit
 ) : BindableItem<ItemSubredditBinding>() {
 
+  private val placeholder = ColorDrawable(Color.LTGRAY)
+
   override fun getLayout(): Int {
     return R.layout.item_subreddit
   }
 
   override fun bind(binding: ItemSubredditBinding, position: Int) {
     binding.title.text = subreddit.title
-    if (subreddit.thumbnail != Uri.EMPTY) {
+    if (subreddit.thumbnail == Uri.EMPTY) {
+      binding.thumbnail.setImageDrawable(placeholder)
+    } else {
       Glide
         .with(binding.root.context)
         .load(subreddit.thumbnail)
+        .placeholder(placeholder)
         .into(binding.thumbnail)
     }
   }
