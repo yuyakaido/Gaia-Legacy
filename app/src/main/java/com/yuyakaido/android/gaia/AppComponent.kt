367ec1a25a0e07db59e37b294a137dfa2793255b
Yuya Kaido <yuyakaido@gmail.com>
2019-12-01 23:43:17 +0900

    Add AppScope for application scope instances

--- app/src/main/java/com/yuyakaido/android/gaia/AppComponent.kt
+++ app/src/main/java/com/yuyakaido/android/gaia/AppComponent.kt
@@ -2,6 +2,7 @@ package com.yuyakaido.android.gaia
 
 import dagger.Component
 
+@AppScope
 @Component(modules = [NetworkModule::class])
 interface AppComponent {
   fun inject(gaia: Gaia)
---
+++ app/src/main/java/com/yuyakaido/android/gaia/AppScope.kt
@@ -0,0 +1,7 @@
+package com.yuyakaido.android.gaia
+
+import javax.inject.Scope
+
+@Scope
+@Retention(value = AnnotationRetention.RUNTIME)
+annotation class AppScope
