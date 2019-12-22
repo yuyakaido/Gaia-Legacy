ab6f8b3dc8ccc242b6a0aec3bae836149a3824fd
Yuya Kaido <yuyakaido@gmail.com>
2019-12-22 20:14:16 +0800

    Show basic info in community detail screen

--- features/community-detail/src/main/java/com/yuyakaido/android/gaia/community/detail/CommunityDetailActivity.kt
+++ features/community-detail/src/main/java/com/yuyakaido/android/gaia/community/detail/CommunityDetailActivity.kt
@@ -3,7 +3,10 @@ package com.yuyakaido.android.gaia.community.detail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.lifecycle.observe
+import com.bumptech.glide.Glide
 import com.yuyakaido.android.gaia.community.detail.databinding.ActivityCommunityDetailBinding
 import com.yuyakaido.android.gaia.core.domain.entity.Community
 import com.yuyakaido.android.gaia.core.presentation.BaseActivity
@@ -34,7 +37,36 @@ class CommunityDetailActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
+    setupToolbar()
+    setupDetail()
     viewModel.onBind()
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      android.R.id.home -> finish()
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  private fun setupToolbar() {
+    supportActionBar?.title = getString(R.string.community_name, getCommunity().name)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+  }
+
+  private fun setupDetail() {
+    viewModel.community
+      .observe(this) { community ->
+        Glide.with(this)
+          .load(community.banner)
+          .into(binding.banner)
+        Glide.with(this)
+          .load(community.icon)
+          .circleCrop()
+          .into(binding.icon)
+        binding.name.text = getString(R.string.community_name, community.name)
+        binding.subscribers.text = getString(R.string.community_subscribers, community.subscribers)
+      }
   }
 
 }
--- features/community-detail/src/main/java/com/yuyakaido/android/gaia/community/detail/CommunityDetailViewModel.kt
+++ features/community-detail/src/main/java/com/yuyakaido/android/gaia/community/detail/CommunityDetailViewModel.kt
@@ -2,11 +2,11 @@ package com.yuyakaido.android.gaia.community.detail
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.yuyakaido.android.gaia.core.domain.entity.Community
 import com.yuyakaido.android.gaia.core.infrastructure.RedditAuthApi
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 class CommunityDetailViewModel @Inject constructor(
@@ -15,10 +15,12 @@ class CommunityDetailViewModel @Inject constructor(
   private val api: RedditAuthApi
 ) : AndroidViewModel(application) {
 
+  val community = MutableLiveData<Community.Detail>()
+
   fun onBind() {
     viewModelScope.launch {
       val detail = api.about(path = summary.name).toEntity()
-      Timber.d(detail.toString())
+      community.value = detail
     }
   }
 
