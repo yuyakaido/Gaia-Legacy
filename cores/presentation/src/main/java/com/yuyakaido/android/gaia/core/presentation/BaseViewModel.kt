2f77f179bd69593af676f383e07859a324fc135d
Yuya Kaido <yuyakaido@gmail.com>
2020-08-29 15:53:28 +0900

    Add CallSuper annotation to lifecycle callback methods in BaseViewModel

--- cores/presentation/src/main/java/com/yuyakaido/android/gaia/core/presentation/BaseViewModel.kt
+++ cores/presentation/src/main/java/com/yuyakaido/android/gaia/core/presentation/BaseViewModel.kt
@@ -1,6 +1,7 @@
 package com.yuyakaido.android.gaia.core.presentation
 
 import android.app.Application
+import androidx.annotation.CallSuper
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
@@ -11,21 +12,27 @@ abstract class BaseViewModel(
   application: Application
 ) : AndroidViewModel(application), LifecycleObserver {
 
+  @CallSuper
   @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
   open fun onCreate() = printLog(Lifecycle.Event.ON_CREATE)
 
+  @CallSuper
   @OnLifecycleEvent(Lifecycle.Event.ON_START)
   open fun onStart() = printLog(Lifecycle.Event.ON_START)
 
+  @CallSuper
   @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
   open fun onResume() = printLog(Lifecycle.Event.ON_RESUME)
 
+  @CallSuper
   @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
   open fun onPause() = printLog(Lifecycle.Event.ON_PAUSE)
 
+  @CallSuper
   @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
   open fun onStop() = printLog(Lifecycle.Event.ON_STOP)
 
+  @CallSuper
   @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
   open fun onDestroy() = printLog(Lifecycle.Event.ON_DESTROY)
 
