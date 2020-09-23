7717ef6c15a5899c24bcf97bcae286855755b3cb
Yuya Kaido <yuyakaido@gmail.com>
2020-09-23 20:22:47 +0900

    Print log on lifecycle callback method of fragment

--- cores/presentation/src/main/java/com/yuyakaido/android/gaia/core/presentation/BaseFragment.kt
+++ cores/presentation/src/main/java/com/yuyakaido/android/gaia/core/presentation/BaseFragment.kt
@@ -1,8 +1,10 @@
 package com.yuyakaido.android.gaia.core.presentation
 
 import android.os.Bundle
+import androidx.lifecycle.Lifecycle
 import com.yuyakaido.android.gaia.core.domain.app.AppNavigatorType
 import dagger.android.support.DaggerFragment
+import timber.log.Timber
 import javax.inject.Inject
 
 abstract class BaseFragment<VM : BaseViewModel> : DaggerFragment(), BaseViewType<VM> {
@@ -15,12 +17,38 @@ abstract class BaseFragment<VM : BaseViewModel> : DaggerFragment(), BaseViewType
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    printLog(Lifecycle.Event.ON_CREATE)
     bindLifecycle(lifecycle)
   }
 
+  override fun onStart() {
+    super.onStart()
+    printLog(Lifecycle.Event.ON_START)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    printLog(Lifecycle.Event.ON_RESUME)
+  }
+
+  override fun onPause() {
+    super.onPause()
+    printLog(Lifecycle.Event.ON_PAUSE)
+  }
+
+  override fun onStop() {
+    super.onStop()
+    printLog(Lifecycle.Event.ON_STOP)
+  }
+
   override fun onDestroy() {
+    printLog(Lifecycle.Event.ON_DESTROY)
     unbindLifecycle(lifecycle)
     super.onDestroy()
+  }
+
+  private fun printLog(event: Lifecycle.Event) {
+    Timber.v("${this::class.java.simpleName}.${event.name} is called.")
   }
 
 }
