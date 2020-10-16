7aa316a9906eadd0461250b9576eb4d2a9940ccb
Yuya Kaido <yuyakaido@gmail.com>
2020-10-16 18:12:52 +0900

    Revert "Change base theme to no actionbar"
    This reverts commit 5606643069e3912dc9858080dd0d0b2a72f796fa.

--- app/src/main/java/com/yuyakaido/android/gaia/AppActivity.kt
+++ app/src/main/java/com/yuyakaido/android/gaia/AppActivity.kt
@@ -35,7 +35,7 @@ class AppActivity : BaseActivity<AppViewModel>() {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
     setupNavigation()
-    setupToolbar()
+    setupActionBar()
     setupBottomNavigationView()
   }
 
@@ -70,8 +70,7 @@ class AppActivity : BaseActivity<AppViewModel>() {
     controller.graph = graph
   }
 
-  private fun setupToolbar() {
-    setSupportActionBar(binding.toolbar)
+  private fun setupActionBar() {
     val configuration = AppBarConfiguration(
       setOf(
         R.id.fragment_article_list,
