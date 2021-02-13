39617aa1be02cc3e3bcbbcde07a95bbd9d510d19
Yuya Kaido <yuyakaido@gmail.com>
2021-02-14 03:15:45 +0900

    Fix DomainState

--- cores/core/src/main/java/com/yuyakaido/android/gaia/core/DomainState.kt
+++ cores/core/src/main/java/com/yuyakaido/android/gaia/core/DomainState.kt
@@ -5,24 +5,24 @@ import com.yuyakaido.android.gaia.core.domain.entity.Community
 import com.yuyakaido.android.reduxkit.StateType
 
 data class DomainState(
-  val articles: Set<Article> = emptySet(),
-  val communities: Set<Community> = emptySet()
+  val articles: Map<Article.ID, Article> = emptyMap(),
+  val communities: Map<Community.Name, Community> = emptyMap()
 ) : StateType {
 
   fun findArticleById(id: Article.ID): Article {
-    return articles.first { it.id == id }
+    return articles.getValue(id)
   }
 
   fun findCommunityByName(name: Community.Name): Community {
-    return communities.first { it.name() == name }
+    return communities.getValue(name)
   }
 
   fun updateArticles(newEntities: List<Article>): DomainState {
-    return copy(articles = articles.plus(newEntities))
+    return copy(articles = articles.plus(newEntities.map { it.id to it }))
   }
 
   fun updateCommunities(newEntities: List<Community>): DomainState {
-    return copy(communities = communities.plus(newEntities))
+    return copy(communities = communities.plus(newEntities.map { it.name() to it }))
   }
 
 }
